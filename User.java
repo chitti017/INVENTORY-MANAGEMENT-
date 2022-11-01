6b58bb5569485dd30ea9a449b13bd7fa02a0e3f5
chitti017 <77652770+chitti017@users.noreply.github.com>
2022-11-02 02:52:44 +0530

    Add files via upload

---
+++ User.java
@@ -0,0 +1,15 @@
+package com.example.admin.sayyes;
+
+public class User {
+    public String deptname, email;
+
+    public User(){
+
+    }
+
+    public User(String name, String email) {
+        this.deptname = name;
+        this.email = email;
+
+    }
+}
